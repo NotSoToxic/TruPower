ef2e9f8461c93a7e1cfd76c4f8eb2698918320d0
Sarthak Joshi <sarthak.joshi52@gmail.com>
2025-04-15 23:58:38 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -37,7 +37,7 @@ const Navbar = () => {
           <div className="flex items-center">
             <Link to="/" className="flex items-center space-x-2">
               <Battery className="h-8 w-8 text-blue-600" />
-              <span className="text-xl font-bold text-gray-900">StrEnergy</span>
+              <span className="text-xl font-bold text-gray-900">TruPower</span>
             </Link>
           </div>
 
